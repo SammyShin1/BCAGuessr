595ae96cf7ad1c6c3272d57577c3830939f8b069
SammyShin1 <shinsammy3@gmail.com>
2026-05-18 13:26:26 -0400

    supabase!

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,25 @@
 'use client';
 
 import dynamic from 'next/dynamic';
+import { supabase } from '../lib/supabase';
+import { useEffect } from 'react';
 
-// Dynamically import the map with no SSR
 const Map = dynamic(() => import('../components/Map'), {
   ssr: false,
   loading: () => <p>Loading map...</p>
 });
 
 export default function Page() {
+  useEffect(() => {
+    async function testConnection() {
+      console.log("SUPABASE TESTING:")
+      const { data, error } = await supabase.from('Images').select('*').limit(1)
+      console.log('data:', data)
+      console.log('error:', error)
+    }
+    testConnection()
+  }, [])
+
   return (
     <div>
       <div>Welcome to BCA.</div>
